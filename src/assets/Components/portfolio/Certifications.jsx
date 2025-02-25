09ef077e5d0e605be3df4afd189e04c1a7ef5e4f
AbubakkarSiddhik <abubakkarsiddhiq54321@gmail.com>
2025-02-25 16:17:14 +0530

    Update Certifications.jsx

--- src/assets/Components/portfolio/Certifications.jsx
+++ src/assets/Components/portfolio/Certifications.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import CambridgeImg from "./cambridge.jpg";
+import CambridgeImg from "./Cambridge.jpg";
 import JustriseImg from "./Justrise.jpg";
 import MernImg from "./Mern.jpg";
 import MLfrontendImg from "./MindLusterFrontEnd.jpg";
